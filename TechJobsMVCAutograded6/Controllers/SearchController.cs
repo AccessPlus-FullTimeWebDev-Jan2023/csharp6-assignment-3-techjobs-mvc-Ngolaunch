e33e3af4618afb8c150322e3d650f1d83a55caba
ngohile <ngohile.yakubu@gmail.com>
2023-04-08 23:53:32 -0500

    On It

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -17,6 +17,24 @@ public class SearchController : Controller
         return View();
     }
 
-    // TODO #3 - Create an action method to process a search request and render the updated search views.
+    // TODO #3 - Create an action method to process a search request
+    // and render the updated search views.
+    public IActionResult Results( string searchType, string searchTerm)
+    {
+        List<Job>> jobs = new List<Job> ();
+        if (searchType.ToLower().Equals("all"))
+        {
+             JobData.FindAll();
+        }
+        if (searchType == null)
+        {
+            JobData.FindAll();
+        }
+        else
+        {
+            JobData.FindByColumnAndValue(column,value);
+        }
+    }
+
 }
 
